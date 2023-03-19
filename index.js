bdf1e0a3c6412b1aec8042a193034628b3401416
the-riquelme <riquelmedamiaosilva@gmail.com>
2023-03-19 10:07:57 -0300

    :sparkles: feat: adds base for the bot to run

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const {
+  Client, Events, GatewayIntentBits,
+} = require('discord.js');
 const dotenv = require('dotenv');
 
 dotenv.config();
+const { TOKEN } = process.env;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Login realizado como ${c.user.tag}`);
+});
+
+client.login(TOKEN);
